4fdda45ea91f56b77df9e075a4ebdba6acf2aa7c
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-06-11 15:00:44 +0700

    responsive Login

--- shopping-app/src/components/Input/Input.tsx
+++ shopping-app/src/components/Input/Input.tsx
@@ -4,23 +4,23 @@ interface Props {
   type: React.HTMLInputTypeAttribute
   errorMessage?: string
   placeholder?: string
-  className?: string
+  className: string
   name: string
-  register: UseFormRegister<any>
+  // register: UseFormRegister<any>
   rules?: RegisterOptions
   autoComplete?: string
 }
 
-export default function Input({ type, errorMessage, placeholder, className, name, register, rules }: Props) {
+export default function Input({ type, errorMessage, placeholder, className, name, rules }: Props) {
   return (
-    <div className={className}>
+    <>
       <input
         type={type}
         placeholder={placeholder}
-        className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
-        {...register(name, rules)}
+        className={className}
+        // {...register(name, rules)}
       />
-      <div className='mt-1 min-h-[1rem] text-sm text-red-600'>{errorMessage}</div>
-    </div>
+      <div className='text-red-600 mt-1 min-h-[1rem] text-sm'>{errorMessage}</div>
+    </>
   )
 }
--- shopping-app/src/pages/Login/Login.tsx
+++ shopping-app/src/pages/Login/Login.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form'
+import Input from '../../components/Input'
 
 export default function Login() {
   const {
@@ -16,40 +17,42 @@ export default function Login() {
     <div className='bg-bgFormLogin py-6'>
       <div className='container'>
         <div className='grid grid-cols-12'>
-          <div className='col-span-8 col-start-3'>
-            <div className='flex justify-between'>
+          <div className='col-span-12 md:col-span-10 md:col-start-2 xl:col-span-8 xl:col-start-3 '>
+            <div className='flex-row sm:flex sm:justify-between'>
               <div className='text-xl'>Chào mừng đến với Lazada. Đăng nhập ngay!</div>
-              <div className='pt-4 text-[10px]'> Thành viên mới? Đăng ký tại đây</div>
+              <div className='pt-3 text-[10px]'> Thành viên mới? Đăng ký tại đây</div>
             </div>
-            <form className='my-8 bg-white px-6 py-8'>
-              <div className='grid grid-cols-12'>
-                <div className='col-span-7'>
-                  <div className='mr-8'>
+            <form className='my-8 rounded-sm bg-white px-8 py-8 sm:px-24 md:px-6 '>
+              <div className='flex-row md:grid md:grid-cols-12'>
+                <div className='md:col-span-7'>
+                  <div className='md:mr-8'>
                     <div>
                       <label htmlFor='' className='text-[12px]'>
-                        Số điện thoại hoặc email*
+                        Email*
                       </label>
-                      <input
-                        placeholder='Vui lòng nhập điện thoại hoặc email của bạn'
+                      <Input
+                        name='email'
+                        placeholder='Vui lòng nhập email của bạn'
                         type='text'
-                        className='mt-2 w-full rounded-sm border border-gray-300 p-2 text-sm text-gray-500 outline-none'
+                        className='mt-2 w-full rounded-sm border border-gray-300 p-2 text-sm outline-emerald-400/50'
                       />
                     </div>
-                    <div className='mt-6'>
+                    <div className='mt-2'>
                       <label htmlFor='' className='text-[12px]'>
                         Mật khẩu*
                       </label>
-                      <input
+                      <Input
+                        name='password'
                         placeholder='Vui lòng nhập mật khẩu của bạn'
                         type='text'
-                        className='mt-2 w-full rounded-sm border border-gray-300 p-2 text-sm outline-none'
+                        className='mt-2 w-full rounded-sm border border-gray-300 p-2 text-sm outline-emerald-400/50'
                       />
                     </div>
-                    <div className='mt-5 flex justify-end text-[12px] text-teal-500/80'>Quên mật khẩu?</div>
+                    <div className='my-3 flex justify-end text-[12px] text-teal-500/80'>Quên mật khẩu?</div>
                   </div>
                 </div>
-                <div className='col-span-5'>
-                  <div className='px-4'>
+                <div className='md:col-span-5'>
+                  <div className='md:px-2 '>
                     <div>
                       <button className='mt-4 w-full rounded-sm bg-orange py-4 text-center uppercase text-white hover:bg-orangeHover'>
                         Đăng nhập
@@ -57,11 +60,16 @@ export default function Login() {
                     </div>
                     <div className='py-2 text-[12px]'>Hoặc đăng nhập bằng</div>
                     <div>
-                      <button className=' w-full rounded-sm bg-blue py-3 text-center text-white'>Facebook</button>
+                      <button className=' w-full rounded-sm bg-blue pb-2 pt-1 text-center text-white'>
+                        <span className='mr-6 text-2xl font-bold'>f</span>
+                        <span>Facebook</span>
+                      </button>
                     </div>
                     <div>
-                      <button className=' bg-red-900 mt-3 w-full rounded-sm bg-red py-3 text-center text-white'>
-                        Google
+                      <button className=' bg-red-900 mt-3 w-full rounded-sm bg-red pb-2 pt-1 text-center text-white'>
+                        <span className=' text-2xl font-bold '>G</span>
+                        <span className='mr-5 text-xl font-bold '>+</span>
+                        <span>Google</span>
                       </button>
                     </div>
                   </div>
